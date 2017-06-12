846d65f611bdac42d4cbeb84f6d46380765f096f
sakibrahmancuet <sakib.cse11.cuet@gmail.com>
2017-06-12 06:58:53 +0600

    Create config.php

--- config.php
+++ config.php
@@ -8,10 +8,10 @@
 
 Class Config{
     const APP_NAME ="adshacker_messenger_bot";
-	const APP_VERSION = 1.0;
+    const APP_VERSION = 1.0;
     const APP_BASE_PATH = "http://localhost/adshacker_messenger/";
-    const APP_ID ="648953695301935";
-    const APP_SECRET = "d19bc976118ad18bf0e90dae4a97ab15";
+    const APP_ID ="140670229825183";
+    const APP_SECRET = "dd8d7c2acea814c8085fb798e2ae845e";
     static $permissions =  ["manage_pages","pages_show_list"];
     const GRAPH_VERSION = "v2.9";
 }
